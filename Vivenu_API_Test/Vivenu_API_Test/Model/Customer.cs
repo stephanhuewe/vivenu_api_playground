5cb384ecb2ffd0a487e6148c56b6d03e1871d166
Stephan Hüwe <stephanhuewe@schaeferhunde.de>
2022-03-21 15:51:52 +0100

    Initial Version

---
+++ Vivenu_API_Test/Vivenu_API_Test/Model/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Vivenu_API_Test.Model
+{
+    internal class Customer
+    {
+        public string name { get; set; }
+        public string prename { get; set; }
+        public string lastname { get; set; }
+        public string image { get; set; }
+        public string primaryEmail { get; set; }
+        public Location location { get; set; }
+        public string notes { get; set; }
+        public ExtraFields extraFields { get; set; }
+        public List<string> dataFields { get; set; }
+        public List<string> tags { get; set; }
+        public string accountId { get; set; }
+    }
+
+    public class Location
+    {
+        public string street { get; set; }
+        public string postal { get; set; }
+        public string city { get; set; }
+        public string locale { get; set; }
+        public string state { get; set; }
+        public List<double> center { get; set; }
+        public string country { get; set; }
+    }
+
+    public class ExtraFields
+    {
+    }
+}
---
+++ Vivenu_API_Test/Vivenu_API_Test/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Vivenu_API_Test.Model;
+
+namespace Vivenu_API_Test
+{
+    internal class Program
+    {
+        private const string URL = "https://vivenu.com/api";
+        private const string TOKEN = "key_<getyourown>";
+
+        static void Main(string[] args)
+        {
+            // Basic information
+            // Docs: https://docs.vivenu.dev/
+            // Base - URL: https://vivenu.com/api/
+            // Authentication via Bearer Token API_KEY (Create it from dashboard)
+
+            // Get all customers sample
+            GetAllCustomers();
+
+            // Create Customer
+            CreateCustomer();
+
+            // Get all customers again
+            GetAllCustomers();
+
+        }
+
+        private static void CreateCustomer() {
+            string url = $"{URL}/customers";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers["Authorization"] = $"Bearer {TOKEN}";
+            httpRequest.ContentType = "application/json";
+            
+            // The documentation says that primary mail is enough
+            // This is not the case at 03/21/2022
+            var data = @"{
+                ""prename"": ""Tom"",
+                ""lastname"": ""Tester"",
+                ""primaryEmail"": ""trash12345@temptrash.com"",
+                ""location"": {
+                    ""street"": ""Test 1234"",
+                    ""postal"": ""03096"",
+                    ""city"": ""Burg"",
+                    ""locale"": ""Germany"",
+                    ""state"": ""Brandenburg"",
+                    ""country"": ""DE""
+                }
+            }";
+
+            // ToDo: Use Model instead
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine(httpResponse.StatusCode);
+        }
+
+        private static void GetAllCustomers() {
+
+            string url = $"{URL}/customers/rich";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "GET";
+
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers["Authorization"] = $"Bearer {TOKEN}";
+            httpRequest.ContentType = "";
+
+
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException())) {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine(httpResponse.StatusCode);
+        }
+    }
+}
